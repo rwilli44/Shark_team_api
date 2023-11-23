e683d8155c5d63656735e114a834cf0fa155e7c2
Tarik SADKHI <tarik.sadkhi@gmail.com>
2023-11-23 15:07:29 +0100

    commentaire class base

--- src/models/commentaire.py
+++ src/models/commentaire.py
@@ -1,5 +1,15 @@
 from base import Base
+from sqlalchemy import ForeignKey, String, Date
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column
 
 
 class Commentaire(Base):
-    pass
+    __tablename__ = "commentaire"
+    
+    id_commentaire: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    date_publication_commentaire: Mapped[Date] = mapped_column(Date())
+    auteur_commentaire: Mapped[str] = mapped_column(String(255))
+    titre_commentaire: Mapped[str] = mapped_column(String(255))
+    id_client: Mapped[int] = mapped_column(ForeignKey("client.id_client"))
+    id_ouvrage: Mapped[int] = mapped_column(ForeignKey("ouvrage.id_ouvrage"))
