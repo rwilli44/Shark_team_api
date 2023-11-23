9b60858929c2e8656002f49f7920a743ebc19f5c
Rachel Williams <rachelwilliams@mailbox.org>
2023-11-23 16:02:17 +0100

    Created models theme and theme_ouvrage

--- src/models/theme.py
+++ src/models/theme.py
@@ -1,5 +1,9 @@
 from base import Base
+from sqlalchemy import String
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column
 
 
 class Theme(Base):
-    pass
+    id_theme: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    nom_theme: Mapped[str] = mapped_column(String(255))
--- src/models/theme_ouvrage.py
+++ src/models/theme_ouvrage.py
@@ -1,5 +1,12 @@
 from base import Base
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import ForeignKey
 
 
 class ThemeOuvrage(Base):
-    pass
+    id_ouvrage: Mapped[int] = mapped_column(
+        ForeignKey("ouvrage.id_ouvrage"), primary_key=True
+    )
+    id_theme: Mapped[int] = mapped_column(
+        ForeignKey("ouvrage.id_ouvrage"), primary_key=True
+    )
