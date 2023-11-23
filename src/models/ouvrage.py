a531df4f48ce603bb0d0dddc70c640ec8afd0c2d
Damien Vialla de Soleyrol <damienvialla@gmail.com>
2023-11-23 15:42:38 +0100

    classe commentaire ok

--- src/models/ouvrage.py
+++ src/models/ouvrage.py
@@ -1,5 +1,25 @@
 from base import Base
-
+from sqlalchemy import ForeignKey, String, Date
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column, relationship
+from commentaire import Commentaire
 
 class Ouvrage(Base):
-    pass
+    __tablename__ = "ouvrage"
+    
+    id_ouvrage: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    titre_ouvrage: Mapped[str] = mapped_column(String(255))
+    auteur_ouvrage: Mapped[str] = mapped_column(String(255))
+    isbn_ouvrage: Mapped[str] = mapped_column(String(20))
+    langue_ouvrage: Mapped[str] = mapped_column(String(20))
+    prix_ouvrage: Mapped[float] = mapped_column(float)
+    date_parution_ouvrage: Mapped[Date] = mapped_column(Date)
+    categorie_ouvrage: Mapped[str] = mapped_column(String(255))
+    date_disponibilite_libraire_ouvrage: Mapped[Date] = mapped_column(Date)
+    date_disponibilite_particulier_ouvrage: Mapped[Date] = mapped_column(Date)
+    image_ouvrage: Mapped[str] = mapped_column(String(255))
+    table_des_matieres_ouvrage : Mapped[str] = mapped_column(String(255))
+    mot_cle_ouvrage : Mapped[str] = mapped_column(String(255))
+    description_ouvrage : Mapped[str] = mapped_column(String(255))
+    commentaires: Mapped[list["Commentaire"]] = relationship(back_populates="ouvrage")
+
