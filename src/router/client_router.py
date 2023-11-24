09ceb639d314f16e609de3ade5183209de0a4eea
Damien Vialla de Soleyrol <damienvialla@gmail.com>
2023-11-24 14:16:44 +0100

    correction theme ouvrage + client schema + client router

--- src/router/client_router.py
+++ src/router/client_router.py
@@ -2,42 +2,27 @@ from fastapi import APIRouter
 from sqlalchemy.orm import Session
 from models.client import Client
 from config.connexion import ENGINE
+from schema.client_schema import Client_schema
+from fastapi.encoders import jsonable_encoder
 
 router = APIRouter()
 
 
 # creation d'une route pour remplire la Bdd table client
-@router.post(
-    "/inscription/{nom}&{prenom}&{email}&{telephone}&{preferences}&{adresse_livraison}&{adresse_facturation}",
-    tags=["client"],
-)
-async def ajout_client(
-    nom: str,
-    prenom: str,
-    email: str,
-    telephone: str,
-    preferences: str,
-    adresse_livraison: str,
-    adresse_facturation: str,
-):
+@router.post("/inscription/")
+async def ajout_client(client : Client_schema):
     with Session(ENGINE) as session:
-        personne = Client(
-            nom_client=nom,
-            prenom_client=prenom,
-            email_client=email,
-            telephone_client=telephone,
-            preferences_client=preferences,
-            adresse_livraison_client=adresse_livraison,
-            adresse_facturation_client=adresse_facturation,
-        )
+        personne = Client(**client.dict())
         session.add_all([personne])
         session.commit()
-    return {
-        "nom :": nom,
-        "Prenom :": prenom,
-        "Email :": email,
-        "telephone :": telephone,
-        "preferences :": preferences,
-        "adresse de livraison :": adresse_livraison,
-        "adresse de facturation : ": adresse_facturation,
-    }
+    return {"client ajouté":personne}  
+
+# creation d'une route pour modifier la Bdd table client
+@router.patch("/modification_client/{client_id}",response_model=Client_schema)
+async def modif_client(client_id : int, client : Client_schema):
+    stored_client_data = client[client_id] # recuperation des données via id
+    stored_client_model = Client_schema(**stored_client_data) # on transforme les données client en Client_schema
+    update_data = client.dict(exclude_unset=True) # transforme données de la requète en dictionnaire
+    update_client =stored_client_model.copy(update=update_data) # application de la mise à jour partielle à l'objet existant
+    client[client_id] = jsonable_encoder(update_client) # mise à jour BdD avec la version mise à jour
+    return update_client # retour au client de l'objet mis à jour
