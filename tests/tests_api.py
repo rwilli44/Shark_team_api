fef0a01f3c0c90c99991444eb042fac0f0c32c90
Rachel Williams <rachelwilliams@mailbox.org>
2023-11-24 23:47:34 +0100

    started writing Commentaire tests

--- tests/tests_api.py
+++ tests/tests_api.py
@@ -10,11 +10,18 @@ class BaseTestFastAPI(unittest.TestCase):
         cls.client = TestClient(app)
 
 
-# class TestFastAPIWithDependencies(BaseTestFastAPI):
-#     def test_route_with_dependency(self):
-#         response = self.client.get("/items/42")
-#         self.assertEqual(response.status_code, 200)
-#         self.assertEqual(response.json(), {"item_id": 42})
+class TestCommentaires(BaseTestFastAPI):
+    def test_add_comment(self):
+        comment_data = {
+            "date_publication_commentaire": "2011-11-20T22:32:46.310Z",
+            "contenu_commentaire": "Testing contents",
+            "titre_commentaire": "Testing contents",
+            "id_client": 2,
+            "id_ouvrage": 1,
+        }
+        response = self.client.post("/commentaires", json=comment_data)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), comment_data)
 
 
 if __name__ == "__main__":
